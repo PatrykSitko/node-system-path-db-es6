4af258c445e3f9031a23aad065383c9974a7b296
Patryk <patryk.sitko.algemeen@gmail.com>
2021-01-30 13:32:40 +0100

    Renamed registerUser to createUser. Added read, update, deleteKeys, delete user functions. Added userCache cleaning; The user cache get's cleaned whenever the user file is no longer present.

--- SystemPathDB.userFunctions.mjs
+++ SystemPathDB.userFunctions.mjs
@@ -1,4 +1,4 @@
-import fs from "fs";
+import formatMail from "./modules/formatMail.mjs";
 /** Has access to the this keyword of the SystemPathDB class.
  * This function gets invoced inside of the SystemPathDB constructor.
  */
@@ -32,31 +32,35 @@ export function updateUsers() {
                   }
                 } while (readFunctionNotAvailable);
 
-                const userCacheData = this.userCache[user];
-                if (
-                  !this.userLock.includes(user) &&
-                  userCacheData &&
-                  JSON.stringify(userData) !== JSON.stringify(userCacheData)
-                ) {
-                  this.userLock.push(user);
-                  await users[user].write(JSON.stringify(userCacheData));
-                  this.userLock.splice(this.userLock.indexOf(user), 1);
-                } else {
-                  this.userCache[user] = userData;
-                }
+                // const userCacheData = this.userCache[user];
+                // if (
+                //   !this.userLock.includes(user) &&
+                //   userCacheData &&
+                //   JSON.stringify(userData) !== JSON.stringify(userCacheData)
+                // ) {
+                //   this.userLock.push(user);
+                //   await users[user].write(JSON.stringify(userCacheData));
+                //   this.userLock.splice(this.userLock.indexOf(user), 1);
+                // } else {
+                this.userCache[user] = userData;
+                // }
                 resolved();
               }.bind(this)
             );
           }
         }
+        Object.keys(this.userCache).forEach((key) =>
+          users.list().includes(key) ? null : delete this.userCache[key]
+        );
         resolve((this.updatingUsers = false));
       }.bind(this)
     );
   }
 }
+
 export default function SystemPathDBUserFunctions() {
   this.addStructureFunction(
-    "registerUser",
+    "createUser",
     async ({ getDatabaseStructure }, email, registrationData = {}) => {
       if (!getDatabaseStructure().includes("users")) {
         await getDatabaseStructure().new("users");
@@ -68,6 +72,56 @@ export default function SystemPathDBUserFunctions() {
         });
       }
       return getDatabaseStructure().users.includes(`${email}_USER`);
+    }
+  );
+  this.addStructureFunction(
+    "readUser",
+    ({ getDatabaseStructure }, emailOrObjectKey) => {
+      const emailOrObjectKeyCopy = formatMail(emailOrObjectKey);
+      const users = getDatabaseStructure().users;
+      if (users && users.includes(emailOrObjectKeyCopy)) {
+        return getDatabaseStructure().users[emailOrObjectKeyCopy].read();
+      }
+    }
+  );
+  this.addStructureFunction(
+    "updateUser",
+    async (
+      { getDatabaseStructure },
+      emailOrObjectKey,
+      registrationData = {}
+    ) => {
+      const emailOrObjectKeyCopy = formatMail(emailOrObjectKey);
+      const users = getDatabaseStructure().users;
+      if (users && users.includes(emailOrObjectKeyCopy)) {
+        getDatabaseStructure().users[emailOrObjectKeyCopy].write({
+          ...(await getDatabaseStructure().readUser(emailOrObjectKeyCopy)),
+          ...registrationData,
+        });
+      }
+    }
+  );
+  this.addStructureFunction(
+    "deleteUserKeys",
+    ({ getDatabaseStructure }, emailOrObjectKey, keysToDelete = []) => {
+      const emailOrObjectKeyCopy = formatMail(emailOrObjectKey);
+      const users = getDatabaseStructure().users;
+      if (users && users.includes(emailOrObjectKeyCopy)) {
+        const userCacheCopy = { ...this.userCache[emailOrObjectKeyCopy] };
+        keysToDelete.forEach((key) => delete userCacheCopy[key]);
+        getDatabaseStructure().users[emailOrObjectKeyCopy].write(userCacheCopy);
+      }
+    }
+  );
+  this.addStructureFunction(
+    "deleteUser",
+    ({ getDatabaseStructure }, emailOrObjectKey) => {
+      const emailOrObjectKeyCopy = formatMail(emailOrObjectKey);
+      const users = getDatabaseStructure().users;
+      if (users && users.includes(emailOrObjectKeyCopy)) {
+        delete this.userCache[emailOrObjectKeyCopy];
+        getDatabaseStructure().users[emailOrObjectKeyCopy].delete();
+      }
     }
   );
 }
